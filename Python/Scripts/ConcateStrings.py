995fdb80420dc3710036daa42eed81a318cfa34f
Chirag Nagrekar <chiragce17@gmail.com>
2024-02-02 19:13:46 +0530

    Create ConcateStrings.py
    Join function in string

---
+++ Python/Scripts/ConcateStrings.py
@@ -0,0 +1,24 @@
+# Using the + operator
+
+str1 = "Hello"
+str2 = "World"
+str = str1 + ' ' + str2
+print(str)
+
+
+# Using the Join function
+str1 = "Hello"
+str2 = "Python"
+str3 = "World"
+str = ','.join([str1,str2,str3])
+print(str)
+
+# Concat list.
+list = ["Dog","cat","cow"]
+str = ' '.join(list)
+print(str)
+
+# Concat tupple 
+wild_animals = ('Tiger','Lion','Fox')
+str = ' '.join(wild_animals)
+print(str)
